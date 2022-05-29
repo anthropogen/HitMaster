a117fcdc64541b7a671d3ab068532bcc902e0e6b
Komarov Alexander <75695365+anthropogen@users.noreply.github.com>
2022-05-29 17:15:52 +0300

    animations , gun, enemy

--- Assets/Source/Scripts/LevelConfigs/Platform.cs
+++ Assets/Source/Scripts/LevelConfigs/Platform.cs
@@ -1,18 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private Enemy[] enemies;
+    [field: SerializeField] public Path Path { get; private set; }
 }
